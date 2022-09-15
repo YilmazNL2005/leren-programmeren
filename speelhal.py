cf923c0ca621e0916d9cb8cb893b8bbb38a2285c
yilmaz vannoorden <99069863@mydavinci.nl>
2022-09-15 18:13:10 +0200

    (updated speelhal!)

---
+++ speelhal.py
@@ -0,0 +1,37 @@
+ticket_p_p = 7.45 
+
+aantal_personen = int(input("met hoeveel mensen ben je?"))
+
+vip_vr_prijs_per_minuut = 0.074
+
+aantal_minuten_vr = int(input("hoeveel minuten wil je gebruik maken van de vip vr gameset?"))
+
+totaal_a = ticket_p_p * aantal_personen
+totaal_b = vip_vr_prijs_per_minuut * aantal_minuten_vr * aantal_personen
+#totale_prijs = totaal_a + totaal_b
+
+
+print(f"de totale prijs voor de tickets is {ticket_p_p * aantal_personen} euro en voor de vip vr gameset is het {vip_vr_prijs_per_minuut * aantal_minuten_vr * aantal_personen} euro")
+
+
+#totaal_a = round (totaal_a, 2);
+#totaal_b = round (totaal_b, 2);
+print(totaal_a + totaal_b, "euro")
+
+#totale_prijs = round (totale_prijs, 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
