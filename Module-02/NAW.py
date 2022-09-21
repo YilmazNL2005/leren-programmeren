e059e45ec89a6bd484f8ab7b780e64f1b6cffcba
YilmazNL2005 <99069863@mydavinci.nl>
2022-09-21 11:09:33 +0200

    (Naw_opdracht)

---
+++ Module-02/NAW.py
@@ -0,0 +1,17 @@
+#minnetjes boven en onder de informatie 
+#Wat is je naam?
+#Wat is je adres?
+#Wat is je postcode?
+#Wat is je woonplaats?
+
+
+naam = input("Wat is je naam?")
+adres = input("Wat is je adres?")
+postcode = input("Wat is je postcode?")
+woonplaats = input("Wat is je woonplaats?")
+print("---------------------------------------------------------------")
+print ("|", "    Naam       :    ", '', '', '', naam)
+print ("|", "    Adres      :    ", '', '', '', adres)
+print ("|", "    Postcode   :    ", '', '', '', postcode)
+print ("|", "    Woonplaats :    ", '', '', '', woonplaats)
+print("----------------------------------------------------------------")
