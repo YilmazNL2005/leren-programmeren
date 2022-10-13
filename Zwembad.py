10ac781d87da435bf9a60a43a88dc8fe9cee3b29
YilmazNL2005 <99069863@mydavinci.nl>
2022-10-13 09:30:00 +0200

    Stap_1_Zwembad

---
+++ Zwembad.py
@@ -0,0 +1,18 @@
+#Kosten voor een zwembad:
+#
+#Voorrijkosten (afhankelijk van de afstand en grootte van het zwembad)
+#Kosten voor het uitgraven (gerekend in m3 (kubieke meters))
+#Kosten voor het afvoeren van de grond (gerekend per m3)
+#Kosten voor het aanbrengen van de wand en vloer (gerekend per m2 (vierkante meter) afhankelijk van de grootte van het bad.)
+#
+#
+
+print("36")
+
+
+
+
+
+
+
+
