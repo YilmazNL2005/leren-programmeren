9b825810435e7dfda94013b8a917e76bd11610ea
YilmazNL2005 <99069863@mydavinci.nl>
2022-11-01 12:41:22 +0100

    Raise_toegevoegd

--- Module-02/Sollicitatie_Opdracht.py
+++ Module-02/Sollicitatie_Opdracht.py
@@ -28,19 +28,30 @@ if vraag_geslacht == "man":
     if vraag_snor == "ja":
         vraag_breedte_snor = int(input("Hoe breed is uw snor in cm? "))
 
+elif vraag_geslacht == "transgender":
+    raise NameError("Dan ben je niet welkom!")
+
+
 elif vraag_geslacht == "vrouw":
-    vraag_haarkleur = input("Wat voor haarkleur heeft u? ")
-    vraag_lengte_haar = int(input("Hoelang is uw haar in cm? "))
+        vraag_haarkleur = input("Wat voor haarkleur heeft u? ")
+        vraag_lengte_haar = int(input("Hoelang is uw haar in cm? "))
 
 
 vraag_diploma = input("Bent u in het bezit van een Diploma MBO-4 ondernemen? ")
+if vraag_diploma == "nee":
+    raise NameError("Ben je dom?")
+
 vraag_rijbewijs = input("Heeft u een vrachtwagen rijbewijs? ")
 vraag_hoed = input("Heeft u een hoge hoed? ")
 vraag_certificaat = input("Heeft u de certificaat 'Overleven met gevaarlijk personeel? ")
 vraag_dieren_dressuur = int(input("Hoeveel jaar praktijkervaring heeft u met dieren-dressuur? "))
 vraag_jongleren = int(input("Hoeveel jaar praktijkervaring heeft u met jongleren? "))
 vraag_acrobatiek = int(input("Hoeveel jaar praktijkervaring heeft u met acrobatiek? "))
+
 vraag_lichaamsgewicht = int(input("Wat is uw lichaamsgewicht in kg? "))
+if vraag_lichaamsgewicht >= 140:
+    raise NameError("Bent u zo zwaar?")
+
 vraag_lengte = int(input("Hoelang bent u in cm? "))
 
 if (vraag_geslacht == "man" and vraag_snor == "ja" and vraag_breedte_snor >= 10 or vraag_geslacht == "vrouw" and vraag_haarkleur == "rood krulhaar" and vraag_lengte_haar >= 20) and vraag_diploma == "ja" and vraag_rijbewijs == "ja" and vraag_hoed == "ja" and vraag_certificaat == "ja" and (vraag_dieren_dressuur >= 4 or vraag_jongleren >= 5 or vraag_acrobatiek >= 3) and 120 >= vraag_lichaamsgewicht >= 90 and 220 >= vraag_lengte >= 150:
