0cdfdf6160f95bf6604dd793748e602f7f0037dd
YilmazNL2005 <99069863@mydavinci.nl>
2022-12-03 23:26:34 +0100

    robotArm10_toegevoegd

---
+++ Module-03/robotarm-python-2021-main/robotArm10.py
@@ -0,0 +1,20 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 10')
+
+# Jouw python instructies zet je vanaf hier:
+
+verplaatsingen = 9
+
+while verplaatsingen > 0:
+    robotArm.grab()
+    for x in range(verplaatsingen):
+        robotArm.moveRight()
+    robotArm.drop()
+    for x in range(verplaatsingen):
+        robotArm.moveLeft()
+    verplaatsingen = verplaatsingen - 2
+    robotArm.moveRight()
+ 
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
