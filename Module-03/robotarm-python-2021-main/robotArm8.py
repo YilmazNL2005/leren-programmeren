9f15b69ed3f1dfca06c2b29a987c75cfa23a2461
YilmazNL2005 <99069863@mydavinci.nl>
2022-12-03 22:10:57 +0100

    robotArm8_toegevoegd

--- Module-03/robotarm-python-2021-main/robotArm8.py
+++ Module-03/robotarm-python-2021-main/robotArm8.py
@@ -4,8 +4,15 @@ robotArm = RobotArm('exercise 8')
 
 # Jouw python instructies zet je vanaf hier:
 #max 11 regels inclusief: Import, robotArm.wait(), het laden van de robotArm = 8 regels max nu!!!
-
-
+    
+for i in range(7):
+        robotArm.moveRight()
+        robotArm.grab()        
+        for i in range(8):
+             robotArm.moveRight()
+        robotArm.drop()
+        for i in range(9):    
+            robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
