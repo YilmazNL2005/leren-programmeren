6d41bde18f6eda381a73555d99d7d5fe14b66cd0
YilmazNL2005 <99069863@mydavinci.nl>
2022-12-04 17:12:57 +0100

    robotArm12_toegevoegd

---
+++ Module-03/robotarm-python-2021-main/robotArm12.py
@@ -0,0 +1,29 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 12')
+robotArm.speed = 4
+# Jouw python instructies zet je vanaf hier:
+verkeerde_blok = 1
+verplaatsingen = 1
+while verplaatsingen <= 10:
+    robotArm.grab()
+    robotArm.scan()
+    if robotArm.scan() == "red":
+        for x in range(0,1):
+            robotArm.moveRight()
+            robotArm.drop()
+    elif robotArm.scan() != "red" or verkeerde_blok == 9:
+        robotArm.drop()
+        robotArm.moveRight()
+        verkeerde_blok += 1
+        if verkeerde_blok == 10:
+            verplaatsingen = 11
+    verplaatsingen += 1
+    if verplaatsingen == 10:
+        verplaatsingen -= 9
+        verkeerde_blok = 1
+        for z in range(9):
+            robotArm.moveLeft()
+    
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
