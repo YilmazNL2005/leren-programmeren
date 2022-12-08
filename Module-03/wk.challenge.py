758a3d2f1683645ee281802bc2e5ebb63e2463d5
YilmazNL2005 <99069863@mydavinci.nl>
2022-12-08 09:46:09 +0100

    wk_challengeVraag1_2.0

--- Module-03/wk.challenge.py
+++ Module-03/wk.challenge.py
@@ -1,8 +1,12 @@
-land1 = "NL"
-land2 = "AR"
-land3 = "VS"
+land1 = input("Welk land speelt in groep A? ")
+land2 = input("Welk land speelt in groep A? ")
+land3 = input("Welk land speelt in groep A? ")
+gewonnen = 3
+verloren = 0
+uitslag = int
+
 
 print("Wedstrijd  #   thuis  -  uit  #   Doelpunten land 1   #   Doelpunten land 2   #   Winnaar  ")
-print(f"    1           {land1}   -   {land3}                                                                             ")
+print(f"    1           {land1}   -   {land3}                                                                            ")
 print(f"    2           {land2}   -   {land1}                                                                            ")
 print(f"    3           {land3}   -   {land2}                                                                            ")
